f4038e3828d1266952c69ac9387df52d7b50969f
Serg Skol <sps676@gmail.com>
2022-11-30 16:58:03 +0100

    Add jest test for countComments

---
+++ src/module/test/count-comments.test.js
@@ -0,0 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+import countComments from '../count-comments.js';
+
+describe('Check add new comments and count it', () => {
+  test('if jest work correctly', () => {
+    expect(true).toBe(true);
+  });
+
+  test('Add comment 1', () => {
+    document.body.innerHTML = `
+    <div class="meal-popup-comments-container"></div>
+  `;
+    const parent = document.querySelector('.meal-popup-comments-container');
+    const row = document.createElement('p');
+    row.innerHTML = '2022-11-30 Anna: Good';
+    parent.appendChild(row);
+
+    expect(countComments(parent)).toBe(1);
+  });
+
+  test('Add comment 2', () => {
+    const parent = document.querySelector('.meal-popup-comments-container');
+    const row = document.createElement('p');
+    row.innerHTML = '2022-11-30 Dima: Great';
+    parent.appendChild(row);
+
+    expect(countComments(parent)).toBe(2);
+  });
+});
