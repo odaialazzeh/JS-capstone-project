8939d6b7c01cc49f30bc887a06acb9e9129e4e82
odaialazzeh <odai.alazzeh@gmail.com>
2022-12-01 20:02:06 +0200

    Update src folder

--- src/module/show-popup-contacts.js
+++ src/module/show-popup-contacts.js
@@ -11,13 +11,16 @@ const showPopupContacts = () => {
     cont.remove();
   });
 
+  const p1 = document.createElement('h2');
+  p1.textContent = 'Contacts:';
+  cont.appendChild(p1);
+
   const div = document.createElement('div');
   div.className = 'cont-popup-div';
   cont.appendChild(div);
 
-  const p1 = document.createElement('h2');
-  p1.textContent = 'Contacts:';
-  div.appendChild(p1);
+  const div1 = document.createElement('div');
+  div.appendChild(div1);
 
   const p2 = document.createElement('p');
   p2.className = 'cont-popup-p';
@@ -25,14 +28,20 @@ const showPopupContacts = () => {
   👤 Odai Alazzeh <br>
       GitHub: @odaialazzeh <br>
       Twitter: @odaialazzeh4 <br>
-      <a href="https://www.linkedin.com/in/odai-alazzeh-01546024a/">LinkedIn</a> <br>
-      <br>
+      <a href="https://www.linkedin.com/in/odai-alazzeh-01546024a/">LinkedIn</a>`;
+  div1.appendChild(p2);
+
+  const div2 = document.createElement('div');
+  div.appendChild(div2);
+
+  const p3 = document.createElement('p');
+  p3.className = 'cont-popup-p';
+  p3.innerHTML = ` 
   👤 Sergiy Shkolnik <br>
-    GitHub: @SergSkol <br>
-    Twitter: @sps676 <br>
-    <a href="https://www.linkedin.com/in/sergiy-shkolnik-7801a53/">LinkedIn</a> <br>
-    `;
-  div.appendChild(p2);
+  GitHub: @SergSkol <br>
+  Twitter: @sps676 <br>
+  <a href="https://www.linkedin.com/in/sergiy-shkolnik-7801a53/">LinkedIn</a>`;
+  div2.appendChild(p3);
 
   mainSection.appendChild(cont);
 };
